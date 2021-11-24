39f8ad8e74238d7ba452cb35edbee051c6ca48a9
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-11-25 01:36:28 +0300

    Исправлено 1-2

--- C++ 1.2 7вариант.cpp
+++ C++ 1.2 7вариант.cpp
@@ -2,19 +2,26 @@
 #include <cmath>
 using namespace std;
 
+double sideOfATriangle(double a, b, gr) // c
+    return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
+
+double areaOfATriangle(double a, b, c){ // S
+    pp = (a+b+c)/2;
+    return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+}
+
+double radius(double a, b, c, S) // R
+    return (a*b*c)/(4*S);
+
 void main(){
-    double a, b, c, gr, S, R;
+    double a, b, gr;
     cout << "a = "; cin >> a;
     cout << "b = "; cin >> b;
     cout << "Угол между ними = "; cin >> gr;
     
-    c = sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
-    double pp = (a+b+c)/2;
-    S = sqrt(pp*(pp-a)*(pp-b)*(pp-c));
-    R = (a*b*c)/(4*S);
-    
-    cout << "a=" << a << " b=" << b << " c=" << c << endl;
-    cout << "S=" << S << " R=" << R << endl << endl;
+    cout << "a=" << a << " b=" << b << " c=" << sideOfATriangle(a, b, gr) << endl;
+    cout << "S=" << areaOfATriangle(a, b, sideOfATriangle(a, b, gr)) << endl;
+    cout << "R=" << radius(a, b, sideOfATriangle(a, b, gr), areaOfATriangle(a, b, sideOfATriangle(a, b, gr))) << endl;
 
     return 0;
 }
