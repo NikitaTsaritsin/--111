a1c9efbb30632d4c257881bd15d2b861e73f4ba0
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-11-25 01:51:55 +0300

    Complete Task-3-1

---
+++ Task-3-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void output(double x){
+    try{
+        double y;
+        y = sqrt(1 - x) - cos(sqrt(1 - x));
+        cout << "x = " << x << "\t\ty = " << y << endl;
+    } catch (exception& e){
+        cout << "Решения нет!" << endl;
+    }
+}
+
+void func(double x, double x2, double s){
+    while (x != (x2 + s)-1){
+        if (x <= x2){
+            output(x);
+            x += s;
+        }
+    }
+}
+
+int main(){
+    const double x = 0;
+    const double x2 = 1;
+    const double stap = 0.1;
+
+    func(x,x2,stap);
+
+    return 0;
+}
