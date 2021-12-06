3f9f559a8caf7482479ac63acb32bdda8e6fd72c
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-07 02:32:09 +0300

    Update C++ 1.1 7вариант.cpp

--- C++ 1.1 7вариант.cpp
+++ C++ 1.1 7вариант.cpp
@@ -5,22 +5,18 @@ using namespace std;
 double functionOne(double x, y, z){
     return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2));
 }
-
 double functionTwo(double x, y, z){
     return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2);
-}
-
-void output(double x, y, z){
-    cout << "x=" << x << " y=" << y << " z=" << z << endl;
-    cout << "a=" << functionOne(x, y, z) << endl; 
-    cout << "b=" << functionTwo(x, y, z) << endl; 
 }
 
 int main(){
     const double x = 0.5; 
     const double y = 0.05; 
     const double z = 0.7;
-    output(x, y, z);
+
+    cout << "x=" << x << " y=" << y << " z=" << z << endl;
+    cout << "a=" << functionOne(x, y, z) << endl; 
+    cout << "b=" << functionTwo(x, y, z) << endl; 
 
     return 0;
 }
