825cff4c9531729cfa2fe26159a6eab8b9abe88c
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-09 13:23:23 +0300

    Update Task-3-1.cpp

--- Task-3-1.cpp
+++ Task-3-1.cpp
@@ -2,31 +2,31 @@
 #include <cmath>
 using namespace std;
 
-void output(double x){
-    try{
+void output(const double x){
+    try{ 
         double y;
-        y = sqrt(1 - x) - cos(sqrt(1 - x));
-        cout << "x = " << x << "\t\ty = " << y << endl;
-    } catch (exception& e){
+        y = sqrt(1 - x) - cos(sqrt(1 - x)); 
+        cout << "x = " << x << "\t\ty = " << y << endl; 
+    } catch (exception& e){ 
         cout << "Решения нет!" << endl;
     }
 }
 
-void func(double x, double x2, double s){
-    while (x != (x2 + s)-1){
-        if (x <= x2){
-            output(x);
-            x += s;
+void func(double x, const double x2, const double s){
+    while (x != (x2 + s)-1){    
+        if (x <= x2){           
+            output(x);          
+            x += s;             
         }
     }
 }
 
 int main(){
-    const double x = 0;
-    const double x2 = 1;
-    const double stap = 0.1;
+    const double x = 0;         
+    const double x2 = 1;        
+    const double stap = 0.1;    
 
-    func(x,x2,stap);
+    func(x,x2,stap); 
 
     return 0;
 }
