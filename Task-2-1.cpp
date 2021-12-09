d2c4be5943c23a6565e1dbca1221747d46a730e9
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-09 13:14:27 +0300

    Update Task-2-1.cpp

--- Task-2-1.cpp
+++ Task-2-1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-double func(double A, double N, int score){
-    cout << "A" << score << " = " << A*(score*N) << endl;
-    return A*(score*N);
+double func(const double A, const double N, const int score){
+    double out = A*(score*N); 
+    cout << "A" << score << " = " << out << endl; 
+    return out; 
 }
 
 int main(){
-    double A, N;
-    cout << "Введите переменные A и N соответственно: ";
-    cin >> A >> N;
+    double A, N;                                            
+    cout << "Введите переменные A и N соответственно: ";    
+    cin >> A >> N;                                          
 
-    A = func(A, N, 1);
-    A = func(A, N, 2);
-    A = func(A, N, 3);
+    A = func(A, N, 1); 
+    A = func(A, N, 2); 
+    A = func(A, N, 3); 
   
     return 0;
 }
