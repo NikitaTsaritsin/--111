8f6934ba2db17b15cdd8c49f51cd6ef79c159709
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-09 13:12:15 +0300

    Update C++ 1.3 7вариант.cpp

--- C++ 1.3 7вариант.cpp
+++ C++ 1.3 7вариант.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
 
-double input(){
-    double l, S;
-    cout << "Длина l(м) = "; cin >> l;
-    cout << "Площадь поперечного сечения S(мм) = "; cin >> S;
-    return l, S;
-} 
-
-void func(double p){
-    double l, S = input();
+void func(const double p, const double l, const double S){ 
     cout << "Удельное сопротивление = " << p*(l*1000/S) << endl << endl;
 }
 
 int main(){
-    const double p = 0.028;
-    func(p);
+    const double p = 0.028; 
+    double l, S;                                                
+    cout << "Длина l(м) = "; cin >> l;                          
+    cout << "Площадь поперечного сечения S(мм) = "; cin >> S;   
+    func(p, l, S); 
 
     return 0;
 }
