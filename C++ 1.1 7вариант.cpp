68b00cc3af51bc26fbd19253b1f49cb51b9f01a2
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-09 13:05:42 +0300

    Update C++ 1.1 7вариант.cpp

--- C++ 1.1 7вариант.cpp
+++ C++ 1.1 7вариант.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
+#include <iostream>     
+#include <cmath>        
+using namespace std;    
 
-double functionOne(double x, y, z){
-    return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2));
-}
-double functionTwo(double x, y, z){
-    return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2);
-}
+double functionOne(const double x, const double y, const double z){ 
+    return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2)); 
+} 
+double functionTwo(const double x, const double y, const double z){ 
+    return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2); 
+} 
 
-int main(){
-    const double x = 0.5; 
+int main(){ 
+    const double x = 0.5;  
     const double y = 0.05; 
-    const double z = 0.7;
+    const double z = 0.7;  
 
-    cout << "x=" << x << " y=" << y << " z=" << z << endl;
-    cout << "a=" << functionOne(x, y, z) << endl; 
-    cout << "b=" << functionTwo(x, y, z) << endl; 
+    cout << "x=" << x << " y=" << y << " z=" << z << endl;  
+    cout << "a=" << functionOne(x, y, z) << endl;           
+    cout << "b=" << functionTwo(x, y, z) << endl;           
 
-    return 0;
+    return 0; 
 }
