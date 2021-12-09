906cf4e15ad1fccaeaf7c16205b521485272eaa4
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-09 13:21:06 +0300

    Update Task-2-3.cpp

--- Task-2-3.cpp
+++ Task-2-3.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void calc(double a[]){
-    cout << "S1 = " << a[0] + a[1] << endl;
-    cout << "S2 = " << a[0] + a[2] << endl;
-    cout << "S3 = " << a[1] + a[2] << endl;
+void calc(const double a[]){ 
+    cout << "S1 = " << a[0] + a[1] << endl; 
+    cout << "S2 = " << a[0] + a[2] << endl; 
+    cout << "S3 = " << a[1] + a[2] << endl; 
 }
 
-bool func(double a[]){
-    bool out = false;
-    a[0] + a[1] > 0 ? out = !out : out = out;
-    a[0] + a[2] > 0 ? out = !out : out = out;
-    a[1] + a[2] > 0 ? out = !out : out = out;
-    return out;
+bool func(const double a[]){ 
+    bool out = false; 
+    if(a[0] + a[1] > 0){ out = !out; return out; } 
+    if(a[0] + a[2] > 0){ out = !out; return out; } 
+    if(a[1] + a[2] > 0){ out = !out; return out; } 
+    return out; 
 }
 
 int main(){
-    double a[3];
-    for(int i = 0 ; i < 3 ; i++){
-        cout << "Введите значение " << i+1 << " - ";
-        cin >> a[i];
+    double a[3]; 
+    for(int i = 0 ; i < 3 ; i++){ 
+        cout << "Введите значение " << i+1 << " - ";    
+        cin >> a[i];                                    
     }
 
-    calc(a);
-    cout << (func(a) ? "successfully" : "unsuccessfully");
+    calc(a); 
+    cout << (func(a) ? "successfully" : "unsuccessfully"); 
 
     return 0;
 }
