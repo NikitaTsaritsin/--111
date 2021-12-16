0b5a23a1ad40689f13e9cea7768834d3e0047c50
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-16 15:50:33 +0300

    Update C++ 1.2 7вариант.cpp

--- C++ 1.2 7вариант.cpp
+++ C++ 1.2 7вариант.cpp
@@ -2,15 +2,37 @@
 #include <cmath>
 using namespace std;
 
+/**
+ * \brief Рассчитывает третью сторону треугольника
+ * \param a первая сторона
+ * \param b вторая сторона
+ * \param gr угол между двумя сторонами в радианах
+ * \return третья сторона
+ */
+
 double sideOfATriangle(const double a, const double b, const double gr){
     return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
 }
+/**
+ * \brief Вычисляет площадь треугольника
+ * \param a первая сторона
+ * \param b вторая сторона
+ * \param c третья сторона
+ * \return площадь треугольника
+ */
 
 double areaOfATriangle(const double a, const double b, const double c){
     pp = (a+b+c)/2;
     return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
 }
-
+/**
+ * \brief Вычисляет радиус описанной окружности
+ * \param a первая сторона
+ * \param b вторая сторона
+ * \param c третья сторона
+ * \param S площадь треугольника
+ * \return радиус описанной окружности
+ */
 double radius(const double a, const double b, const double c, const double S){
     return (a*b*c)/(4*S);
 }
@@ -21,6 +43,21 @@ void main(){
     cout << "b = "; cin >> b;                   
     cout << "Угол между ними = "; cin >> gr;    
     
+    /**
+     * \brief Использование функий
+     * \code
+     * double sideOfATriangle(const double a, const double b, const double gr){
+    return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
+}
+     *double areaOfATriangle(const double a, const double b, const double c){
+    pp = (a+b+c)/2;
+    return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+}
+     * double radius(const double a, const double b, const double c, const double S){
+    return (a*b*c)/(4*S);
+}
+     *\endcode
+     */
     cout << "a=" << a << " b=" << b << " c=" << sideOfATriangle(a, b, gr) << endl;                                      
     cout << "S=" << areaOfATriangle(a, b, sideOfATriangle(a, b, gr)) << endl;                                           
     cout << "R=" << radius(a, b, sideOfATriangle(a, b, gr), areaOfATriangle(a, b, sideOfATriangle(a, b, gr))) << endl;  
