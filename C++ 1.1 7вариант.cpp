7d37426f14857890790ef5bc68772d7d48562436
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-18 20:05:26 +0300

    Update C++ 1.1 7вариант.cpp

--- C++ 1.1 7вариант.cpp
+++ C++ 1.1 7вариант.cpp
@@ -9,10 +9,7 @@ using namespace std;
  * \param z параметр z
  * \return значение функции
  */
-
-double functionOne(const double x, const double y, const double z){ 
-    return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2)); 
-} 
+double functionOne(const double x, const double y, const double z); 
 
 /**
  * \brief Это функция выполняет вычисление по заданной формуле
@@ -21,11 +18,7 @@ double functionOne(const double x, const double y, const double z){
  * \param z параметр z
  * \return   значение функции
  */
-
-double functionTwo(const double x, const double y, const double z){ 
-    return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2); 
-} 
-
+double functionTwo(const double x, const double y, const double z);
 
 /**
  * \brief Точка входа в программу
@@ -39,24 +32,15 @@ int main(){
     const double z = 0.7;  
 
     cout << "x=" << x << " y=" << y << " z=" << z << endl; 
-    /**
-     * \brief Использование функий
-     *\code 
-     *double functionOne(const double x, const double y, const double z){ 
-    return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2)); 
-     *\endcode
-     */
-} 
-
     cout << "a=" << functionOne(x, y, z) << endl;  
-    /**
-     * \brief Использование функций
-     *\code
-     *double functionTwo(const double x, const double y, const double z){ 
-    return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2); 
-    *\endcode
-    */         
     cout << "b=" << functionTwo(x, y, z) << endl;           
-
     return 0; 
 }
+
+double functionOne(const double x, const double y, const double z){ 
+    return (pow(x,2)*(x+1))/(y-((1-cos(x+z))/2)); 
+} 
+
+double functionTwo(const double x, const double y, const double z){ 
+    return sqrt((x*y)/z)+((1+cos(pow(x+y,2)))/2); 
+} 
