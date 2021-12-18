b7347c7c8c138e87a754a11aaaa119babb6e8772
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-18 21:48:56 +0300

    Update C++ 1.2 7вариант.cpp

--- C++ 1.2 7вариант.cpp
+++ C++ 1.2 7вариант.cpp
@@ -10,9 +10,7 @@ using namespace std;
  * \return третья сторона
  */
 
-double sideOfATriangle(const double a, const double b, const double gr){
-    return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
-}
+double sideOfATriangle(const double a, const double b, const double gr);
 /**
  * \brief Вычисляет площадь треугольника
  * \param a первая сторона
@@ -21,10 +19,7 @@ double sideOfATriangle(const double a, const double b, const double gr){
  * \return площадь треугольника
  */
 
-double areaOfATriangle(const double a, const double b, const double c){
-    pp = (a+b+c)/2;
-    return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
-}
+double areaOfATriangle(const double a, const double b, const double c);
 /**
  * \brief Вычисляет радиус описанной окружности
  * \param a первая сторона
@@ -33,34 +28,29 @@ double areaOfATriangle(const double a, const double b, const double c){
  * \param S площадь треугольника
  * \return радиус описанной окружности
  */
-double radius(const double a, const double b, const double c, const double S){
-    return (a*b*c)/(4*S);
-}
+double radius(const double a, const double b, const double c, const double S);
 
 void main(){
     double a, b, gr;                            
     cout << "a = "; cin >> a;                   
     cout << "b = "; cin >> b;                   
     cout << "Угол между ними = "; cin >> gr;    
-    
-    /**
-     * \brief Использование функий
-     * \code
-     * double sideOfATriangle(const double a, const double b, const double gr){
-    return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
-}
-     *double areaOfATriangle(const double a, const double b, const double c){
-    pp = (a+b+c)/2;
-    return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
-}
-     * double radius(const double a, const double b, const double c, const double S){
-    return (a*b*c)/(4*S);
-}
-     *\endcode
-     */
     cout << "a=" << a << " b=" << b << " c=" << sideOfATriangle(a, b, gr) << endl;                                      
     cout << "S=" << areaOfATriangle(a, b, sideOfATriangle(a, b, gr)) << endl;                                           
     cout << "R=" << radius(a, b, sideOfATriangle(a, b, gr), areaOfATriangle(a, b, sideOfATriangle(a, b, gr))) << endl;  
 
     return 0;
+}
+
+double sideOfATriangle(const double a, const double b, const double gr){
+    return sqrt(pow(a,2)+pow(b,2)-2*a*b*cos(gr));
+}
+
+double areaOfATriangle(const double a, const double b, const double c){
+    pp = (a+b+c)/2;
+    return sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+}
+
+double radius(const double a, const double b, const double c, const double S){
+    return (a*b*c)/(4*S);
 }
