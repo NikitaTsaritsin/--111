f4bd845bdc3e851f18711e9e560c08f9eaaabc4e
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-22 20:38:43 +0300

    Update Task-2-1.cpp

--- Task-2-1.cpp
+++ Task-2-1.cpp
@@ -4,67 +4,86 @@
 using namespace std;
 
 /**
- * \brief 
+ * \brief Константы вариантов решения.
  */
 enum class Operation
 {
     /**
-     * \brief 
+     * \brief Первый вариант решения
      */
-    one,
+    Case1,
 
-     /**
-      * \brief 
-      */
-    two,
+    /**
+     * \brief Второй вариант решения
+     */
+    Case2,
 
-     /**
-      * \brief 
-      */
-    three
+    /**
+     * \brief Третий вариант решения
+     */
+    Case3,
 };
 
-void oneMultiplication(double &a, const double n);
+/**
+ * \brief функция, меняющая переменные первым вариантом.
+ * \param a - константа, вводимая пользователем
+ * \param n - константа, вводимая пользователем
+  */
+void oneMultiplication(double a, double n);
+/**
+ * \brief функция, меняющая переменные вторым вариантом.
+ * \param a - константа, вводимая пользователем
+ * \param n - константа, вводимая пользователем
+  */
+void twoMultiplication(double a, double n);
+/**
+ * \brief функция, меняющая переменные третьим вариантом.
+ * \param a - константа, вводимая пользователем
+ * \param n - константа, вводимая пользователем
+  */
+void threeMultiplication(double a, double n);
 
-void twoMultiplication(double &a, const double n);
-
-void threeMultiplication(double &a, const double n);
-
-int main(){
+/**
+ * \brief Точка входа в программу.
+ * \return Возвращает 0 в случае успеха.
+ */
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
     double a, n; int score;
     cout << "Введите число A: "; cin >> a;
     cout << "Введите число N: "; cin >> n;
     cout << "Введите кол-во операций (1-3): "; cin >> score;
-    
-    switch(score-1){
-        case static_cast<int>(Operation::one):{
-            oneMultiplication(a, n);
-            break;
-        }
-        case static_cast<int>(Operation::two):{
-            twoMultiplication(a, n);
-            break;
-        }
-        case static_cast<int>(Operation::three):{
-            threeMultiplication(a, n);
-            break;
-        }
-        default:
-            cout << "Ошибка";
+
+    switch (score - 1) {
+    case static_cast<int>(Operation::Case1): {
+        oneMultiplication(a, n);
+        break;
+    }
+    case static_cast<int>(Operation::Case2): {
+        twoMultiplication(a, n);
+        break;
+    }
+    case static_cast<int>(Operation::Case3): {
+        threeMultiplication(a, n);
+        break;
+    }
+    default:
+        cout << "Ошибка";
     }
     cout << "Результат " << n << " операций: " << a << endl;
 
     return 0;
 }
 
-void oneMultiplication(double &a, const double n){
-    a = a*n;
+void oneMultiplication(double a, double n) {
+    a = a * n;
 }
 
-void twoMultiplication(double &a, const double n){
-    a = a*(2*n);
+void twoMultiplication(double a, double n) {
+    a = a * (2 * n);
 }
 
-void threeMultiplication(double &a, const double n){
-    a = a*(3*n);
+void threeMultiplication(double a, double n) {
+    a = a * (3 * n);
 }
