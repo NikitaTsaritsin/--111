640dd1db697f33aecba713a6f89f6e6ad32a6dac
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-22 18:06:24 +0300

    Update C++ 1.3 7вариант.cpp

--- C++ 1.3 7вариант.cpp
+++ C++ 1.3 7вариант.cpp
@@ -1,35 +1,43 @@
+#define USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
+#include <locale>
+#include <string>
+
 using namespace std;
 /**
- * \brief Рассчитывает  сопротивление провода
- * \param p сопротивление
- * \param l длина провода
- * \param S площадь поперечного сечения
+ \brief Рассчитывает сопротивление провода
+ \param p сопротивление
+ \param length длина провода
+ \param Square площадь поперечного сечения
+ \return Рассчитаем сопротивление провода
  */
+double getresistance(const double p, const double length, const double Square);
 
-void func(const double p, const double l, const double S){ 
-    cout << "Удельное сопротивление = " << p*(l*1000/S) << endl << endl;
-}
-
-int main(){
 /**
- * \brief табличное значение физической величины
- * \param p сопротивление Алюминия = (0.028 Ом*мм^2)/м
- */	
-    const double p = 0.028; 
-    double l, S;                                                
-    cout << "Длина l(м) = "; cin >> l;                          
-    cout << "Площадь поперечного сечения S(мм) = "; cin >> S; 
+\brief Точка входа в программу
+\param p сопротивление Алюминия = (0.028 Ом*мм^2)/м
+\return В случае успеха возвращаем 0
+*/
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const double p = 0.028;
+    cout << "Введите длину провода = ";
+    double length;
+    cin >> length;
 
-    /**
-     * \brief Использование функий
-     * \code
-     void func(const double p, const double l, const double S){ 
-    cout << "Удельное сопротивление = " << p*(l*1000/S) << endl << endl;
-}
-    * \endcode
-    */
-    func(p, l, S); 
+    cout << "Введите площадь поперечного сечения = ";
+    double Square;
+    cin >> Square;
 
+    const double resistance = getresistance(p, length, Square);
+    cout << "\n Сопротивление =  " << resistance << endl;
     return 0;
+}
+
+
+double getresistance(const double p, const double length, const double Square)
+{
+    return length * p / Square;
 }
