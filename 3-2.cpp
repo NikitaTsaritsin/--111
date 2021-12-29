dfe7a2d55ffa5ee093bffac89ec2cd57fba7e3fb
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-29 15:55:36 +0300

    Update 3-2.cpp

--- 3-2.cpp
+++ 3-2.cpp
@@ -9,25 +9,21 @@ using namespace std;
 double getCurrent(const int index);
 
 /**
- * \brief Рассчитывает значение факториала числа посредством рекурсии (\a i).
- * \param i Текущий элемент факториала.
- * \return Значение факториала исходного числа.
- */
-double factorial(const double i);
-
-/**
  * \brief Точка входа в программу.
  * \return 0 – в случае успеха,<br/>1 – в противном случае.
  */
 int main(){
     try{
-        int size; cout << "Введите количество элементов последовательности: ";
+        int index = 1;
+        int size; 
+        cout << "Введите количество элементов последовательности: ";
         cin >> size;
         if (size < 1)
             throw invalid_argument("Значение должно быть больше 0!");
-        double summ = 0;
-        for (int i = 1; i < size; i++)
-            summ += getCurrent(i);
+        double summ = 0.0;
+        for (int i = 0; i < size; i++)
+            summ += getCurrent(index);
+            index++;
         cout << "Сумма последовательности из " << size << " элементов равна: " << summ << endl;
         return 0;
     }
@@ -38,10 +34,5 @@ int main(){
 }
 
 double getCurrent(const int index){
-    return index / factorial(index+1);
-}
-
-double factorial(const double i){
-    if (i==0) return 1;
-    else return i*factorial(i-1);
+    return index / (index + 2);
 }
