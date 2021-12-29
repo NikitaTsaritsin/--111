57ae94708e00bffcae0250ee34e27c90f9dfd0e6
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-29 15:52:26 +0300

    Update Task-2-2.cpp

--- Task-2-2.cpp
+++ Task-2-2.cpp
@@ -2,9 +2,58 @@
 #include <cmath>
 using namespace std;
 
+/**
+ * \brief Решение 1го уравнения.
+ * \param a Константа, данная в условии.
+ * \param x Переменная, вводимая пользователем.
+ * \return Значение первого уравнения.
+*/ 
+double first(const double a, const double x);
+
+/**
+ * \brief Решение 2го уравнения.
+ * \param a Константа, данная в условии.
+ * \param x Переменная, вводимая пользователем.
+ * \return Значение второго уравнения.
+*/ 
+double second(const double a, const double x);
+
+/**
+ * \brief Определяет значение переменной y.
+ * \param a Константа, данная в условии.
+ * \param x Переменная, вводимая пользователем.
+ * \param f Номер решаемого уравнения.
+ * \return Значение переменной y.
+*/ 
+double output(const double a, const double x, const int f);
+
+/**
+ * \brief Определяет какое уранение будет решаться.
+ * \param a Константа, данная в условии.
+ * \param x Переменная, вводимая пользователем.
+ * \return Номер уравнения.
+*/ 
+int func(const double a, const double x);
+
+/**
+ * \brief Точка входа в прграмму.
+ * \return Возвращает 0 в случае успеха.
+*/ 
+int main(){
+    const double a = 1.5; 
+    double x;                               
+    cout << "Введите число X: "; cin >> x;  
+
+    cout << "a*x " << (func(a,x)==1 ? ">= " : "< ") << 1 << endl;    
+    cout << "y = " << output(a,x,func(a,x));                        
+  
+    return 0;
+}
+
 double first(const double a, const double x){   
     return a - log10(a*x);
 }
+
 double second(const double a, const double x){   
     return a + log10(a*x);
 }
@@ -13,19 +62,9 @@ double output(const double a, const double x, const int f){
     double out = (f==0 ? first(a,x) : second(a,x)); 
     return out;
 }
+
 int func(const double a, const double x){    
     int f = 0;                   
     if(a*x >= 1) f++;           
     return f;                   
-}
-
-int main(){
-    const double a = 1.5; 
-    double x;                               
-    cout << "Введите число X: "; cin >> x;  
-
-    cout << "ax " << (func(a,x)==1 ? ">= " : "< ") << 1 << endl;    
-    cout << "y = " << output(a,x,func(a,x));                        
-  
-    return 0;
 }
