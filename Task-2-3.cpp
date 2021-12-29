3fd883cee2369bf617285587d0368a2f07b7eaaf
NikitaTsaritsin <92607908+NikitaTsaritsin@users.noreply.github.com>
2021-12-29 15:53:44 +0300

    Update Task-2-3.cpp

--- Task-2-3.cpp
+++ Task-2-3.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * \brief Вычисление суммы 2х значений.
+ * \param a Одномерный статический массив.
+ * \return Значение суммы.
+*/
+void calc(const double a[]);
+
+/**
+ * \brief Определение положительности.
+ * \param a Одномерный статический массив
+ * \return true в случае если сумма положительна, false в случае, если отрицательна.
+*/
+bool func(const double a[]);
+
+/**
+ * \brief Точка входа в программу.
+ * \return Возвращает 0 в случае успеха.
+*/
+int main(){
+    double a[3]; 
+    for(int i = 0; i < 3; i++){ 
+        cout << "Введите значение " << i + 1 << " - ";    
+        cin >> a[i];                                    
+    }
+
+    calc(a); 
+    cout << (func(a) ? "successfully" : "unsuccessfully");
+    return 0;
+}
+
 void calc(const double a[]){ 
     cout << "S1 = " << a[0] + a[1] << endl; 
     cout << "S2 = " << a[0] + a[2] << endl; 
@@ -13,17 +43,4 @@ bool func(const double a[]){
     if(a[0] + a[2] > 0){ out = !out; return out; } 
     if(a[1] + a[2] > 0){ out = !out; return out; } 
     return out; 
-}
-
-int main(){
-    double a[3]; 
-    for(int i = 0 ; i < 3 ; i++){ 
-        cout << "Введите значение " << i+1 << " - ";    
-        cin >> a[i];                                    
-    }
-
-    calc(a); 
-    cout << (func(a) ? "successfully" : "unsuccessfully"); 
-
-    return 0;
 }
